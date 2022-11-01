7daf2ff96d4ff2d87f6776cc4e4683b050df1d59
Alexandra Marian <alexandra.marian02@e-uvt.ro>
2022-11-01 20:07:54 +0200

    Modifed index and tried to add the background img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,18 @@
 import { type NextPage } from "next";
+import Image from "next/image";
+
+const HomePage: NextPage = () =>{
+    return(
+        <div className="fixed w-screen h-screen">
+            <Image
+                className="relative z-0"
+                src = {"/homeBG.png"}
+                alt = ""
+                fill = {true}
+            />
+            <h1 className="text-white text-3xl">Hi</h1>
+        </div>
+    )
+}
+
+export default HomePage
