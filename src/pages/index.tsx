9c3af0aac6c5b943970a5bbdabc1503cfa829230
Alexandra Marian <alexandra.marian02@e-uvt.ro>
2022-11-09 11:55:14 +0200

    Added the icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import { type NextPage } from "next";
 const HomePage: NextPage = () => {
     return (
         <div className="h-screen bg-cover bg-main-page">
-            <nav className="p-10 bg-black opacity-50">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" className="w-6 h-6">
+            <nav className="p-8 bg-black opacity-50 flex items-center justify-between">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" opacity="100"  className="w-12 h-12 ">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" opacity="100" className="w-12 h-12">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                 </svg>
             </nav>
         </div>
