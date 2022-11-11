72ed2809002ebc6f3199cf4f4a2a05f37f08341d
Alexandra Marian <alexandra.marian02@e-uvt.ro>
2022-11-11 20:56:42 +0200

    Added the title and get inspired button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,12 @@ const HomePage: NextPage = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" opacity="100" className="w-12 h-12">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                 </svg>
-            </nav>
+            </nav>  
+            <div className="flex flex-col items-center p-36">
+                <p className="text-center text-white text-8xl">BeHealthy</p>
+                <hr className="my-4 mx-auto w-4/12 h-1 bg-white rounded border-0 md:my-5 dark:bg-gray-700"></hr>
+                <button className="bg-black hover:bg-green-900 text-white font-bold my-5 py-4 px-5 rounded-full text-2xl">Get inspired</button>
+            </div>
         </div>
 
     )
