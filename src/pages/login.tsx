88dcf90cfbcb24a5bc21240bccc3abaf8afbae44
Alexandra Marian <alexandra.marian02@e-uvt.ro>
2022-11-26 21:31:19 +0200

    Co-authored-by: Screciu Alin  <AlinScreciu@users.noreply.github.com>

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,9 +4,9 @@ import {ImGoogle} from "react-icons/im";
 const LoginPage: NextPage = () => {
     return (
         <div className="h-screen bg-cover bg-login-page flex flex-col items-center">
-            <h1 className="text-center text-white text-8xl p-14">LOGIN</h1>
+            <h1 className="text-center text-white text-8xl p-14 font-sans">LOGIN</h1>
             <button type="button" className="text-black bg-[#ffffff] hover:bg-[#4285F4]/90 hover:text-white focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
-                <ImGoogle className="mr-2 -ml-1 w-4 h-4" />
+                <ImGoogle className="mr-2 -ml-1 w-4 h-4 font-sans" />
                 Sign in with Google
             </button>
         </div>
--- src/pages/recommendations.tsx
+++ src/pages/recommendations.tsx
@@ -1,8 +1,66 @@
 import { type NextPage } from "next";
+import { FaUser } from "react-icons/fa";
+import { BiRefresh } from "react-icons/bi";
+import Link from "next/link";
 
+interface RecipeProps {
+    id: string;
+    title: string;
+    img: string;
+    desc: string;
+}
+const randomRecipes: RecipeProps[] = [
+    {
+        id: "1",
+        title: "Ice cream",
+        img: "Icecream.png",
+        desc: "Tasty icecream"
+    },
+    {
+        id: "2",
+        title: "Ice cream",
+        img: "Icecream.png",
+        desc: "Tasty icecream"
+    },
+    {
+        id: "3",
+        title: "Ice cream",
+        img: "Icecream.png",
+        desc: "Tasty icecream"
+    },
+
+]
+const Recipe: React.FC<RecipeProps> = (recipe: RecipeProps) => {
+    return (
+        <div className="grow-0 max-w-xs">
+            <div className="border-2 rounded-md border-black w-72 h-72">
+                <img src={recipe.img} alt="Pic" className="object-fill rounded-md aspect-square" />
+            </div>
+            <h2 className="font-bold break-words text-lg">{recipe.title}</h2>
+            <p className="break-words text-sm">{recipe.desc}</p>
+            <Link href={`/recipe/${recipe.id}`}>
+                <button className="bg-black hover:bg-green-900 text-white font-bold my-5 py-4 px-7 rounded-full text-xl">View</button>
+            </Link>
+        </div>
+    )
+}
 const RecommendationsPage: NextPage = () => {
     return (
-        <h2>Our recommendations</h2>
+        <div className="font-sans">
+            <div className="flex items-center justify-between p-10">
+                <h2 className="text-5xl font-bold">Our recommendations</h2>
+                <div className="flex flex-row space-x-8">
+                    <button type="button" className="text-white hover:bg-green-900 bg-black hover:text-white focus:ring-4 focus:outline-none focus:ring-[#67A07C]/50 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+                        <BiRefresh className="mr-2 -ml-1 w-8 h-8 font-sans" color="white" />
+                        Others
+                    </button>
+                    <FaUser className="w-12 h-12 " color="black" />
+                </div>
+            </div>
+            <div className="font-sans flex flex-row items-center justify-evenly p-16">
+                {randomRecipes.map((recipe, i) => <Recipe {...recipe} />)}
+            </div>
+        </div>
     )
 }
 export default RecommendationsPage
