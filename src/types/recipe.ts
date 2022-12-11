d08bc703e9e4f4c53cdc27936eaac28a440a14d8
Alin Screciu <alin.screciu01@gmail.com>
2022-12-11 21:18:34 +0200

    recipe type move

---
+++ src/types/recipe.ts
@@ -0,0 +1,13 @@
+export type RecipeType = {
+    id: string,
+    Name: string,
+    Author: string,
+    AuthorID: string,
+    Ingredients: string,
+    Details: string,
+    Portions: number,
+    Preparation: string,
+    Cooking: string,
+    Tools: string,
+    ImageURL: string,
+  }
